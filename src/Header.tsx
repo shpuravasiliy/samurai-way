db3380700f6a097c009121650a2c302bcc01bbae
Shpura Vasiliy <shpura.vasiliy@gmail.com>
2022-05-26 11:32:15 +0300

    create new branch - work

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,9 +1,9 @@
 const Header = () => {
     return (
         <div>
-            <a href='#s'>Home</a>
-            <a href='#s'>News Feed</a>
-            <a href='#s'>Messages</a>
+            <a href='#s'>Home</a> -
+            <a href='#s'>News Feed</a> -
+            <a href='#s'>Messages</a> -
         </div>
     );
 }
