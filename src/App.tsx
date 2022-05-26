693587d504893d08b4deff6895ad41188101ce1e
Shpura Vasiliy <shpura.vasiliy@gmail.com>
2022-05-26 11:49:46 +0300

    delete Header, Technologies, Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
-import Header from "./Header";
-import Technologies from "./Technologies";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-        <Header />
-        <Technologies />
+
     </div>
   );
 }
